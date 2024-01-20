583dde7bf5869fcf9509555dbf9541629f197ddd
Lautaro Garcia <lautarojgarcia177@gmail.com>
2024-01-20 11:13:32 -0300

    Funcion suma
    * funcion suma
    
    * prueba suma
    
    * changes after code review

--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,8 @@
+import { suma } from "./suma.js"
+
 console.log("Hola mundo");
 var x = 1;
 var y = 2;
+var z = 3;
+
+console.log(suma(x, y, z));
---
+++ suma.js
@@ -0,0 +1,3 @@
+export function suma(x, y, z) {
+  return x + y + z;
+}
